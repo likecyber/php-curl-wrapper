c28749d488ee095893f1229072f90aa69d3d1f72
Likecyber <cyber2friends@gmail.com>
2017-02-26 14:30:03 +0700

    First commit to branch.
    Added Curl Wrapper Class and Bundle of CA Root Certificates.

---
+++ curl.class.php
@@ -0,0 +1,141 @@
+<?php
+
+class Curl {
+	public $handle;
+
+	public $init_options = array();
+	public $options = array();
+	public $raw_options = array();
+	public $raw_strings = array();
+
+	public $errno = NULL;
+	public $error = NULL;
+	public $info = NULL;
+	public $response = NULL;
+
+	public function __construct($options = NULL) {
+		if (is_object($options) && isset($options->options) && is_array($options->options)) {
+			$this->init_options = $options->options;
+		} elseif (is_array($options)) {
+			$this->init_options = $options;
+		} elseif (is_string($options)) {
+			$this->init_options = array("URL" => $options);
+		}
+		$this->init();
+	}
+
+	public function __call($function, $arguments) {
+		if (function_exists("curl_".$function) && substr($function, 0, 5) !== "multi" && substr($function, 0, 5) !== "share") {
+			if (in_array($function, array("copy_handle", "errno", "error", "escape", "pause", "unescape"))) {
+				array_unshift($arguments, $this->handle);
+			}
+			return call_user_func_array("curl_".$function, $arguments);
+		} else {
+			if (count($arguments) === 1) {
+				if ($this->__set($function, $arguments[0])) {
+					return $this;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public function __set($option, $value) {
+		$_option = $option;
+		if (is_string($option) && substr($option, 0, 8) !== "CURLOPT_") {
+			$option = "CURLOPT_".$option;
+		}
+		if (is_string($option) && defined($option)) {
+			$option = constant($option);
+		}
+		if (is_int($option)) {
+			$this->setopt($_option, $value);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function init($options = NULL) {
+		$this->close();
+		$this->handle = curl_init();
+		$this->RETURNTRANSFER = true; // Override RETURNTRANSFER option.
+		if (file_exists(__DIR__."/cacert.pem")) { // If cacert.pem exists, Override SSL_VERIFYPEER and CAINFO options.
+			$this->SSL_VERIFYPEER = true;
+			$this->CAINFO = realpath(__DIR__."/cacert.pem");
+		}
+		$this->setopt_array($this->init_options);
+		if (is_object($options) && isset($options->options) && is_array($options->options)) {
+			$this->setopt_array($options->options);
+		} elseif (is_array($options)) {
+			$this->setopt_array($options);
+		} elseif (is_string($options)) {
+			$this->setopt("URL", $options);
+		}
+		return $this;
+	}
+
+	public function reset() {
+		curl_reset($this->handle);
+		return $this;
+	}
+
+	public function setopt($option, $value) {
+		$_option = $option;
+
+		if (is_string($option) && substr($option, 0, 8) !== "CURLOPT_") {
+			$option = "CURLOPT_".$option;
+		}
+		if (is_string($option) && defined($option)) {
+			$option = constant($option);
+		}
+		if (isset($this->raw_str[$option])) {
+			unset($this->options[$this->raw_str[$option]]);
+		}
+		curl_setopt($this->handle, $option, $value);
+		$this->options[$_option] = $value;
+		$this->raw_options[$option] = $value;
+		$this->raw_strings[$option] = $_option;
+		return $this;
+	}
+
+	public function setopt_array($options) {
+		foreach ($options as $option => $value) {
+			$this->setopt($option, $value);
+		}
+		return $this;
+	}
+
+	public function getinfo($option = NULL) {
+		if (is_string($option) && substr($option, 0, 9) !== "CURLINFO_") {
+			$option = "CURLINFO_".$option;
+		}
+		if (is_string($option) && defined($option)) {
+			$option = constant($option);
+		}
+		if ($option === NULL) {
+			return curl_getinfo($this->handle);
+		} else {
+			return curl_getinfo($this->handle, $option);
+		}
+	}
+
+	public function exec() {
+		$this->response = curl_exec($this->handle);
+		$this->errno = $this->errno();
+		$this->error = $this->error();
+		$this->info = $this->getinfo();
+		return $this->response;
+	}
+
+	public function close() {
+		@curl_close($this->handle);
+		return true;
+	}
+
+}
+
+?>
